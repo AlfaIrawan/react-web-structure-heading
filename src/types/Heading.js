e1cc9e6b48d5dbd793a9c1359f4799a82cbee997
Alfa Irawan <alfa.irawan@outlook.com>
2019-08-01 17:51:08 +0700

    v0.1.6

--- src/types/Heading.js
+++ src/types/Heading.js
@@ -6,7 +6,7 @@ import '../index.css';
 class Heading extends Component {
     render() {
         return(
-            <div id='id-heading-container' className='heading-container'>
+            <div id='id-heading-container' className='heading-container' style={Object.assign({ marginBottom: 20 }, this.props.containerStyle)}>
                 <h1 id='id-heading-h1' className='heading-h1' style={ this.props.titleStyle }>{this.props.title}</h1>
             </div>
         )
--- src/types/HeadingWithDescription.js
+++ src/types/HeadingWithDescription.js
@@ -6,7 +6,7 @@ import '../index.css';
 class HeadingWithContent extends Component {
     render() {
         return(
-            <div id='id-heading-container' className='heading-container'>
+            <div id='id-heading-container' className='heading-container' style={Object.assign({ marginBottom: 20 }, this.props.containerStyle)}>
                 <h1 id='id-heading-h1' className='heading-h1' style={ this.props.titleStyle }>{this.props.title}</h1>
                 <div id='id-heading-description' className='heading-description' style={ this.props.descriptionStyle }>
                     { this.props.description }
